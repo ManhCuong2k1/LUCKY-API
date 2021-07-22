15041c20b1c9fefb962223e77a9bd65660991840
cuongdm <cuongdm@flextech.vn>
2021-07-22 18:36:47 +0700

    feat add api banner

--- src/controllers/api/admin/AdminController.ts
+++ src/controllers/api/admin/AdminController.ts
@@ -2,8 +2,10 @@ import express from "express";
 const router = express.Router();
 import UserController from "./UserController";
 import LotteryOrderController from "./LotteryOrderController";
+import BannerController from "./BannerController";
 
 router.use("/users", UserController);
 router.use("/lottery-order", LotteryOrderController);
+router.use("/banner", BannerController);
 
 export default router;
---
+++ src/controllers/api/admin/BannerController.ts
@@ -0,0 +1,44 @@
+import { Banner } from "@models/Banner";
+import express, { Response, Request } from "express";
+import { GridInterface } from "@models/Transformers/Grid";
+import { Op } from "sequelize";
+
+const router = express.Router();
+
+/**
+ * Thêm mới banner
+ */
+
+router.post("/", async (req: Request, res: Response) => {
+    try {
+        const banners: any[] = req.body.banners;
+        await Banner.destroy({ truncate: true });
+        await Banner.bulkCreate(banners);
+        res.send("success");
+    } catch (e) {
+        res.status(400).send({
+            error: e.message
+        });
+    }
+});
+
+router.get("/", async (req: Request, res: Response) => {
+    try {
+        const dataBanner = await Banner.findAll({
+            order: [
+                "index"
+            ],
+        });
+        if(dataBanner) {
+            res.send(dataBanner);
+        } else {
+            res.status(400).send('error');
+        }
+    } catch (e) {
+        res.status(400).send({
+            error: e.message
+        });
+    }
+});
+
+export default router;
