e1576b769d33fe341fa2af1f32c3c21fc5c93fb3
lucvd-6158 <lucvd@flextech.vn>
2021-08-17 14:37:06 +0700

    update init settings

--- src/controllers/api/init/initController.ts
+++ src/controllers/api/init/initController.ts
@@ -37,23 +37,23 @@ router.get("/app-settings", async (req: Request, res: Response) => {
         });
 
         dataExport.exchange = {};
-        dataExport.exchange.local_min = await getSettings("exchange_local_min");
-        dataExport.exchange.local_max = await getSettings("exchange_local_max");
-        dataExport.exchange.momo_min = await getSettings("exchange_momo_min");
-        dataExport.exchange.momo_max = await getSettings("exchange_momo_max");
-        dataExport.exchange.vnpay_min = await getSettings("exchange_vnpay_min");
-        dataExport.exchange.vnpay_max = await getSettings("recharge_vnpay_max");
-        dataExport.exchange.bank_min = await getSettings("exchange_bank_min");
-        dataExport.exchange.bank_max = await getSettings("exchange_bank_max");
+        dataExport.exchange.local_min = Number(await getSettings("exchange_local_min"));
+        dataExport.exchange.local_max = Number(await getSettings("exchange_local_max"));
+        dataExport.exchange.momo_min = Number(await getSettings("exchange_momo_min"));
+        dataExport.exchange.momo_max = Number(await getSettings("exchange_momo_max"));
+        dataExport.exchange.vnpay_min = Number(await getSettings("exchange_vnpay_min"));
+        dataExport.exchange.vnpay_max = Number(await getSettings("recharge_vnpay_max"));
+        dataExport.exchange.bank_min = Number(await getSettings("exchange_bank_min"));
+        dataExport.exchange.bank_max = Number(await getSettings("exchange_bank_max"));
 
         dataExport.recharge = {};
-        dataExport.recharge.momo_min = await getSettings("recharge_momo_min");
-        dataExport.recharge.momo_max = await getSettings("recharge_momo_max");
-        dataExport.recharge.vnpay_min = await getSettings("recharge_vnpay_min");
-        dataExport.recharge.vnpay_max = await getSettings("recharge_vnpay_max");
+        dataExport.recharge.momo_min = Number(await getSettings("recharge_momo_min"));
+        dataExport.recharge.momo_max = Number(await getSettings("recharge_momo_max"));
+        dataExport.recharge.vnpay_min = Number(await getSettings("recharge_vnpay_min"));
+        dataExport.recharge.vnpay_max = Number(await getSettings("recharge_vnpay_max"));
         
         dataExport.ticket = {};
-        dataExport.ticket.ticket_storage_fee = await getSettings("ticket_storage_fee");
+        dataExport.ticket.ticket_storage_fee = Number(await getSettings("ticket_storage_fee"));
 
         dataExport.support = {};
         dataExport.support.phone = await getSettings("hot_line");
