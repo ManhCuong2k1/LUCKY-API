7c72ef68651e6e666486fadfed89ba19cfd9bc1d
lucvd-6158 <lucvd@flextech.vn>
2021-08-22 22:57:52 +0700

    add module utils pushNotity to client app

---
+++ src/util/push-notify.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PushNotify = async (to: string, title: string, body: string) => {
+
+    const data:any = JSON.stringify({
+        "to": to,
+        "notification": {
+            "title": title,
+            "body": body,
+            "sound": "camel.caf"
+        }
+    });
+
+    const sendNotity = await axios({
+        method: 'post',
+        url: 'https://fcm.googleapis.com/fcm/send',
+        headers: {
+            'Authorization': 'key=' + process.env.FCM_KEY,
+            'Content-Type': 'application/json'
+        },
+        data: data
+    });
+
+    return sendNotity;
+}
+
+
+export default PushNotify;
